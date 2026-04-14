a157253d33223366dd75c30bac04b97dad05253e
Danilevz <antondanilevskiy405@gmail.com>
2026-04-14 12:40:30 +0300

    changed code №1 practice

--- задачи/Словари/practice 14 april/№1) slovar test.py
+++ задачи/Словари/practice 14 april/№1) slovar test.py
@@ -1,11 +1,16 @@
 user=dict()
-user['name']=input('enter name')
-user['surname']=input('enter surname')
-user['age']=int(input('enter age'))
+user['name']=input('enter name__')
+user['surname']=input('enter surname__')
+user['age']=int(input('enter age__'))
+user['kurs']=input('enter kurs__')
+print(user)
+user.setdefault('group', 'IITO')
 print(user)
 user['age']=19
 print(user)
-del user['name']
+user['kurs']=2
+print(user)
+del user['age']
 print(user)
 user.setdefault('email', None)
 print(user)
